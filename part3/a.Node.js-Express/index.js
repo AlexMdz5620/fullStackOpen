f8b8943d62585543a2178a65347f0e5d678bb778
Alex Mdz <ichialex5620mail.com>
2024-08-28 10:27:25 -0600

    puros fallos

--- part3/a.Node.js-Express/index.js
+++ part3/a.Node.js-Express/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json());
-app.use(express.static('dist'))
 
 let notes = [
   {
